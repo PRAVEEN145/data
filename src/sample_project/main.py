0035bbd9a2a1436d5c563236942c80495cfbb1ca
Govindu Praveen <praveenroyal060p.r@gmail.com>
2026-08-11 12:53:24 +0530

    initial version

--- src/sample_project/main.py
+++ src/sample_project/main.py
@@ -1,2 +1,6 @@
+from .test_folder.re_use import sample_re_use_function
+
 def main():
     print("Hello World")
+
+    sample_re_use_function()
---
+++ src/sample_project/test_folder/re_use.py
@@ -0,0 +1,6 @@
+def sample_re_use_function():
+    """
+    This is a sample function to demonstrate code reuse.
+    It can be imported and used in other modules.
+    """
+    print("This is a reusable function from re_use.py")
